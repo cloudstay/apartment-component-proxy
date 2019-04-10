7d91b94dd5870cf37880650dacf0244c48b1696c
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-04-10 10:39:27 -0700

    Add get request for reviews component

--- server.js
+++ server.js
@@ -15,11 +15,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/rooms/:id', express.static(path.join(__dirname, '/public')));
 
+// photos
+app.get('/api/rooms/:id/photos', (req, response) => {
+  request(`http://localhost:3006/api/rooms/${req.params.id}/photos`, (err, res, body)=> {
+    if(err){
+      response.status(404);
+      response.end()
+    }else {
+      response.status(200);
+      response.end(body)
+    }
+  });
+});
+
+// listings
 app.get('/api/rooms/:id/listing', (req, res) => {
   console.log(req.query.id);
   request(`http://localhost:3003/api/rooms/${req.params.id}/listing`, (err, response, body ) => {
     if(err) {
-      console.log(err)
       res.send(404);
     } else{
       res.status(200).send(body);
@@ -27,20 +40,31 @@ app.get('/api/rooms/:id/listing', (req, res) => {
   });
 });
 
+// reviews
+app.get('/api/rooms/:id/reviews/', (req, response) => {
+  request(`http://localhost:3004/api/rooms/${req.params.id}/reviews`, (err, res, body)=> {
+    if(err){
+      response.status(404);
+      response.end()
+    }else {
+      response.status(200);
+      response.end(body)
+    }
+  });
+});
+
+app.get('/api/rooms/:id/searchReviews', (req, response) => {
+  request(`http://localhost:3004/api/rooms/${req.params.id}/searchReviews`, (err, res, body)=> {
+    if(err){
+      response.status(404);
+      response.end()
+    }else {
+      response.status(200);
+      response.end(body)
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
